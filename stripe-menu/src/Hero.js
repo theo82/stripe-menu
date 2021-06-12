64d0f892d8458af928f65fb27a451b5bd5005ea2
theo82 <theoaristi53@gmail.com>
2021-06-12 18:23:43 +0300

    Context Boilerplate

--- stripe-menu/src/Hero.js
+++ stripe-menu/src/Hero.js
@@ -1,6 +1,8 @@
 import React from 'react'
-
+import { useGlobalContext } from './context'
 const Hero = () => {
+  const data = useGlobalContext()
+  console.log(data)
   return <h1>Hero</h1>
 }
 
---
+++ stripe-menu/src/context.js
@@ -0,0 +1,44 @@
+import React, { useState, useContext } from 'react'
+import sublinks from './data'
+
+const AppContext = React.createContext()
+
+export const AppProvider = ({ children }) => {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false)
+  const [isSubmenuOpen, setIsSubmenuOpen] = useState(false)
+
+  const openSidebar = () => {
+    setIsSidebarOpen(true)
+  }
+
+  const closeSidebar = () => {
+    setIsSidebarOpen(false)
+  }
+
+  const openSubmenu = (text, coordinates) => {
+    setIsSubmenuOpen(true)
+  }
+
+  const closeSubmenu = () => {
+    setIsSubmenuOpen(false)
+  }
+
+  return (
+    <AppContext.Provider
+      value={{
+        isSidebarOpen,
+        isSubmenuOpen,
+        openSidebar,
+        openSubmenu,
+        closeSidebar,
+        closeSubmenu,
+      }}
+    >
+      {children}
+    </AppContext.Provider>
+  )
+}
+
+export const useGlobalContext = () => {
+  return useContext(AppContext)
+}
